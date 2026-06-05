d01835d54fc04fdb6e3516ac4243a72c28f9a8cc
Nils <78668364+Nils1024@users.noreply.github.com>
2026-06-05 14:10:02 +0200

    Initial commit

---
+++ src/main/java/de/nils/iplocatorapi/IpLocatorApiApplication.java
@@ -0,0 +1,13 @@
+package de.nils.iplocatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpLocatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpLocatorApiApplication.class, args);
+    }
+
+}
