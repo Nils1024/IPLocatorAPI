0424f62b53c703b31c7c2a80eeed80caaf63b3ce
Nils <78668364+Nils1024@users.noreply.github.com>
2026-06-21 02:50:50 +0200

    Added pre ip validation

--- src/main/java/de/nils/iplocatorapi/controllers/IpController.java
+++ src/main/java/de/nils/iplocatorapi/controllers/IpController.java
@@ -2,10 +2,13 @@ package de.nils.iplocatorapi.controllers;
 
 import de.nils.iplocatorapi.daos.IPData;
 import de.nils.iplocatorapi.services.DataService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.sql.SQLException;
 
 @RestController
@@ -17,11 +20,24 @@ public class IpController {
     }
 
     @GetMapping("/v1/ip/{ip}")
-    public IPData getIp(@PathVariable String ip) {
+    public ResponseEntity<IPData> getIp(@PathVariable String ip) {
+        if(!isValidIp(ip)) {
+            return ResponseEntity.badRequest().build();
+        }
+
         try {
-            return dataService.getIPData(ip);
+            return ResponseEntity.ok().body(dataService.getIPData(ip));
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isValidIp(String ip) {
+        try {
+            InetAddress.getByName(ip);
+            return true;
+        } catch(UnknownHostException e) {
+            return false;
         }
     }
 
